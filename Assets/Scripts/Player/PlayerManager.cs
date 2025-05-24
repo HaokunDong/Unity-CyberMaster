7822f8a594212549f7bc7db7b4d3c4f953356bfd
GriffinDoUrden <g445615105@gmail.com>
2025-05-24 19:17:10 +0900

    PlayerManager恢复

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -8,10 +8,26 @@ using UnityEngine;
 public class PlayerManager : SingletonComp<PlayerManager>
 {
     public Player player;
+    public GameObject playerPrefab;
 
     private void Start()
     {
         TableTest().Forget();
+    }
+
+    public Player CreatePlayer(GameObject playerSpawnPoint)
+    {
+        if (player != null)
+        {
+            return player;
+        }
+        if (player == null && playerPrefab != null)
+        {
+            var obj = GameObject.Instantiate(playerPrefab, playerSpawnPoint.transform.position, Quaternion.identity);
+            player = obj.GetComponent<Player>();
+            return player;
+        }
+        return null;
     }
 
     public static async UniTask TableTest()
