4062c21754fad2f3894fc701df3ce58c06ec2515
Barry <644680768@qq.com>
2025-06-23 21:04:19 +0800

    新添加了一个10003关卡

--- Assets/Scripts/BaseLib/GameFacade.cs
+++ Assets/Scripts/BaseLib/GameFacade.cs
@@ -61,7 +61,7 @@ public class GameFacade : MonoBehaviour
     #region 初始化结束后
     private async Task OnInitEnd()
     {
-        var gpObj = await ResourceManager.LoadAssetAsync<GameObject>(GamePlayTable.GetTableData(10001).Prefab, ResType.Prefab);
+        var gpObj = await ResourceManager.LoadAssetAsync<GameObject>(GamePlayTable.GetTableData(10003).Prefab, ResType.Prefab);
         var gp = gpObj.GetComponent<GamePlayRoot>();
         gp.Init().Forget();
     }
