f8fd2cd2726780a8034e7bddaa9003380dc2e50e
GriffinDoUrden <g445615105@gmail.com>
2025-07-29 11:51:05 +0900

    框架推进
    解决键盘模拟时转身迟缓的问题

--- Assets/Scripts/Input/SmoothDirectionInput.cs
+++ Assets/Scripts/Input/SmoothDirectionInput.cs
@@ -10,10 +10,11 @@ public class SmoothDirectionInput
     private float acceleration;
     private float deceleration;
     private float rawInput;
-    private bool isAnalog = false;
+    private bool isAnalog = false;  // 是否是模拟输入（非数字输入），如手柄摇杆等
 
     private float minStartValue;   // 起步时最小值
     private float deadThreshold;   // 死区值
+    private float turnAcceleration;  // 方向相反时的转向加速度
 
     /// <summary>
     /// 
@@ -22,12 +23,14 @@ public class SmoothDirectionInput
     /// <param name="deceleration">平滑插值减速度</param>
     /// <param name="minStartValue">开始加速平滑插值时最小值</param>
     /// <param name="deadThreshold">开始减速平滑插值时小于一定阈值后归0</param>
-    public SmoothDirectionInput(float acceleration, float deceleration, float minStartValue, float deadThreshold)
+    /// <param name="turnAcceleration">方向相反时的转向加速度</param>
+    public SmoothDirectionInput(float acceleration, float deceleration, float minStartValue, float deadThreshold, float turnAcceleration = -1f)
     {
         this.acceleration = acceleration;
         this.deceleration = deceleration;
         this.minStartValue = minStartValue;
         this.deadThreshold = deadThreshold;
+        this.turnAcceleration = turnAcceleration <= 0f ? deceleration : turnAcceleration; // 默认转向加速度是停止加速度
     }
 
     public void SetMinAndMax(float minValue, float maxValue)
@@ -45,6 +48,7 @@ public class SmoothDirectionInput
 
     public void Update(float deltaTime)
     {
+        // 如果是模拟输入（如手柄摇杆），则直接使用原始输入值
         if (isAnalog)
         {
             CurrentValue = rawInput;
@@ -64,20 +68,27 @@ public class SmoothDirectionInput
         // 有输入时加速
         if (Mathf.Abs(rawInput) > 0.01f)
         {
-            // 起步时给一个初始值
+            // 如果当前速度是 0，直接从 minStartValue 起步
             if (Mathf.Approximately(CurrentValue, 0f))
             {
                 CurrentValue = minStartValue * Mathf.Sign(rawInput);
             }
 
-            CurrentValue = Mathf.MoveTowards(CurrentValue, rawInput, acceleration * deltaTime);
+            // 如果 rawInput 和当前方向相反，表示转向，需要快速转向
+            if (Mathf.Sign(rawInput) != Mathf.Sign(CurrentValue))
+            {
+                CurrentValue = Mathf.MoveTowards(CurrentValue, rawInput, turnAcceleration * deltaTime);
+            }
+            else
+            {
+                CurrentValue = Mathf.MoveTowards(CurrentValue, rawInput, acceleration * deltaTime);
+            }
         }
         else
         {
             // 减速
             CurrentValue = Mathf.MoveTowards(CurrentValue, 0f, deceleration * deltaTime);
 
-            // 速度很小了，直接归零
             if (Mathf.Abs(CurrentValue) < deadThreshold)
             {
                 CurrentValue = 0f;
